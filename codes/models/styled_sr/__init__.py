55b58fb67f011c08c1493639206d0b486072316f
James Betker <jbetker@gmail.com>
2021-09-29 09:21:44 -0600

    Clean up codebase
    Remove stuff that I'm likely not going to use again (or generally failed experiments)

--- codes/models/styled_sr/__init__.py
+++
@@ -1,9 +0,0 @@
-from models.styled_sr.discriminator import StyleSrGanDivergenceLoss
-
-
-def create_stylesr_loss(opt_loss, env):
-    type = opt_loss['type']
-    if type == 'style_sr_gan_divergence_loss':
-        return StyleSrGanDivergenceLoss(opt_loss, env)
-    else:
-        raise NotImplementedError
