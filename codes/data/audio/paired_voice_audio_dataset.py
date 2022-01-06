6706591d3d873a37bbbcad4ae7b9e22e6b78d2c1
James Betker <jbetker@gmail.com>
2022-01-06 15:24:37 -0700

    Fix dataset

--- codes/data/audio/paired_voice_audio_dataset.py
+++ codes/data/audio/paired_voice_audio_dataset.py
@@ -120,7 +120,7 @@ class TextWavLoader(torch.utils.data.Dataset):
         try:
             tseq, wav, text, path = self.get_wav_text_pair(self.audiopaths_and_text[index])
             cond, cond_is_self = load_similar_clips(self.audiopaths_and_text[index][0], self.conditioning_length, self.sample_rate,
-                                      n=self.conditioning_candidates) if self.load_conditioning else None, False
+                                      n=self.conditioning_candidates) if self.load_conditioning else (None, False)
         except:
             if self.skipped_items > 100:
                 raise  # Rethrow if we have nested too far.
