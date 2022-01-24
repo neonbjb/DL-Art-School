e2ed0adbd8ee1cd8e2597eb81e7c2124e3547cd3
James Betker <jbetker@gmail.com>
2022-01-24 14:31:28 -0700

    use_diffuse_tts updates

--- codes/scripts/audio/gen/use_diffuse_tts.py
+++ codes/scripts/audio/gen/use_diffuse_tts.py
@@ -22,9 +22,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-opt', type=str, help='Path to options YAML file used to train the diffusion model', default='../options/train_diffusion_tts_medium.yml')
     parser.add_argument('-diffusion_model_name', type=str, help='Name of the diffusion model in opt.', default='generator')
-    parser.add_argument('-diffusion_model_path', type=str, help='Path to saved model weights', default='X:\\dlas\\experiments\\train_diffusion_tts_medium\\models\\5200_generator.pth')
+    parser.add_argument('-diffusion_model_path', type=str, help='Path to saved model weights', default='X:\\dlas\\experiments\\train_diffusion_tts_medium\\models\\14800_generator_ema.pth')
     parser.add_argument('-aligned_codes', type=str, help='Comma-delimited list of integer codes that defines text & prosody. Get this by apply W2V to an existing audio clip or from a bespoke generator.',
                         default='0,0,0,0,10,10,0,4,0,7,0,17,4,4,0,25,5,0,13,13,0,22,4,4,0,21,15,15,7,0,0,14,4,4,6,8,4,4,0,0,12,5,0,0,5,0,4,4,22,22,8,16,16,0,4,4,4,0,0,0,0,0,0,0')  # Default: 'i am very glad to see you', libritts/train-clean-100/103/1241/103_1241_000017_000001.wav.
+    # -cond "Y:\libritts/train-clean-100/103/1241/103_1241_000017_000001.wav"
     parser.add_argument('-cond', type=str, help='Path to the conditioning input audio file.', default='Y:\\clips\\books1\\754_Dan Simmons - The Rise Of Endymion 356 of 450\\00026.wav')
     parser.add_argument('-diffusion_steps', type=int, help='Number of diffusion steps to perform to create the generate. Lower steps reduces quality, but >40 is generally pretty good.', default=100)
     parser.add_argument('-output_path', type=str, help='Where to store outputs.', default='../results/use_diffuse_tts')
@@ -38,22 +39,22 @@ if __name__ == '__main__':
     print("Loading data..")
     aligned_codes = torch.tensor([int(s) for s in args.aligned_codes.split(',')]).cuda()
     diffuser = load_discrete_vocoder_diffuser(desired_diffusion_steps=args.diffusion_steps)
-    cond = load_audio(args.cond, 11025).cuda()
-    if cond.shape[-1] > 44000:
-        cond = cond[:,:44000]
+    cond = load_audio(args.cond, 22050).cuda()
+    if cond.shape[-1] > 88000:
+        cond = cond[:,:88000]
 
     with torch.no_grad():
         print("Performing inference..")
         diffusion.eval()
-
-        # Pad MEL to multiples of 4096//spectrogram_compression_factor
-        msl = aligned_codes.shape[-1]
-        dsl = 2048 // aligned_codes_compression_factor
-        gap = dsl - (msl % dsl)
-        if gap > 0:
-            aligned_codes = torch.nn.functional.pad(aligned_codes, (0, gap))  # This still isn't a perfect multiple, but it's close.
         output_shape = (1, 1, ceil_multiple(aligned_codes.shape[-1]*aligned_codes_compression_factor, 2048))
-        output = diffuser.p_sample_loop(diffusion, output_shape, model_kwargs={'tokens': aligned_codes.unsqueeze(0),
-                                                                               'conditioning_input': cond.unsqueeze(0)})
 
-        torchaudio.save(os.path.join(args.output_path, 'output.wav'), output.cpu().squeeze(0), 11025)
+        output = diffuser.p_sample_loop(diffusion, output_shape, noise=torch.zeros(output_shape, device='cuda'),
+                                        model_kwargs={'tokens': aligned_codes.unsqueeze(0),
+                                        'conditioning_input': cond.unsqueeze(0)})
+        torchaudio.save(os.path.join(args.output_path, f'output_mean.wav'), output.cpu().squeeze(0), 11025)
+
+        for k in range(5):
+            output = diffuser.p_sample_loop(diffusion, output_shape, model_kwargs={'tokens': aligned_codes.unsqueeze(0),
+                                                                                   'conditioning_input': cond.unsqueeze(0)})
+
+            torchaudio.save(os.path.join(args.output_path, f'output_{k}.wav'), output.cpu().squeeze(0), 11025)
