fix mel normalization

codes/scripts/audio/gen/speech_synthesis_utils.py
@@ -22,10 +22,10 @@ def load_speech_dvae():
 
 def load_univnet_vocoder():
     model = UnivNetGenerator()
-    sd = torch.load('univnet_c32_pretrained_libri.pt')
-    model.load_state_dict(sd)
+    sd = torch.load('../experiments/univnet_c32_pretrained_libri.pt')
+    model.load_state_dict(sd['model_g'])
     model = model.cpu()
-    model.eval()
+    model.eval(inference=True)
     return model
 
 
@@ -36,12 +36,13 @@ def wav_to_mel(wav, mel_norms_file='../experiments/clips_mel_norms.pth'):
     return TorchMelSpectrogramInjector({'in': 'wav', 'out': 'mel', 'mel_norm_file': mel_norms_file},{})({'wav': wav})['mel']
 
 
-def wav_to_univnet_mel(wav):
+def wav_to_univnet_mel(wav, do_normalization=False):
     """
     Converts an audio clip into a MEL tensor that the univnet vocoder knows how to decode.
     """
     return MelSpectrogramInjector({'in': 'wav', 'out': 'mel', 'sampling_rate': 24000,
-                                   'n_mel_channels': 100, 'mel_fmax': 12000},{})({'wav': wav})['mel']
+                                   'n_mel_channels': 100, 'mel_fmax': 12000,
+                                   'do_normalizattion': do_normalization},{})({'wav': wav})['mel']
 
 
 def convert_mel_to_codes(dvae_model, mel):

codes/trainer/injectors/audio_injectors.py
@@ -7,6 +7,14 @@ import torchaudio
 from trainer.inject import Injector
 from utils.util import opt_get, load_model_from_config
 
+TACOTRON_MEL_MAX = 2.3143386840820312
+TACOTRON_MEL_MIN = -11.512925148010254
+
+def normalize_tacotron_mel(mel):
+    return 2 * ((mel - TACOTRON_MEL_MIN) / (TACOTRON_MEL_MAX - TACOTRON_MEL_MIN)) - 1
+
+def denormalize_tacotron_mel(norm_mel):
+    return ((norm_mel+1)/2)*(TACOTRON_MEL_MAX-TACOTRON_MEL_MIN)+TACOTRON_MEL_MIN
 
 class MelSpectrogramInjector(Injector):
     def __init__(self, opt, env):
@@ -21,14 +29,7 @@ class MelSpectrogramInjector(Injector):
         mel_fmax = opt_get(opt, ['mel_fmax'], 8000)
         sampling_rate = opt_get(opt, ['sampling_rate'], 22050)
         self.stft = TacotronSTFT(filter_length, hop_length, win_length, n_mel_channels, sampling_rate, mel_fmin, mel_fmax)
-        self.mel_norm_file = opt_get(opt, ['mel_norm_file'], None)
-        if self.mel_norm_file is not None:
-            # Note that this format is different from TorchMelSpectrogramInjector.
-            mel_means, mel_max, mel_min, mel_stds, mel_vars = torch.load(self.mel_norm_file)
-            self.mel_max = mel_max
-            self.mel_min = mel_min
-        else:
-            self.mel_max = None
+        self.do_normalization = opt_get(opt, ['do_normalization'], None)  # This is different from the TorchMelSpectrogramInjector. This just normalizes to the range [-1,1]
 
     def forward(self, state):
         inp = state[self.input]
@@ -37,8 +38,8 @@ class MelSpectrogramInjector(Injector):
         assert len(inp.shape) == 2
         self.stft = self.stft.to(inp.device)
         mel = self.stft.mel_spectrogram(inp)
-        if self.mel_max is not None:
-            mel = 2 * ((mel - self.mel_min) / (self.mel_max - self.mel_min)) - 1
+        if self.do_normalization:
+            mel = normalize_tacotron_mel(mel)
         return {self.output: mel}
 
 