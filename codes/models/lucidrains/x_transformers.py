df0cdf1a4f6d54cfc7fcbdd04ff3a99ac58dc83c
James Betker <jbetker@gmail.com>
2022-06-11 08:00:09 -0600

    tfd9 returns with some optimizations

--- codes/models/lucidrains/x_transformers.py
+++ codes/models/lucidrains/x_transformers.py
@@ -352,12 +352,12 @@ class RMSNorm(nn.Module):
 
 
 class RMSScaleShiftNorm(nn.Module):
-    def __init__(self, dim, eps=1e-8):
+    def __init__(self, dim, eps=1e-8, bias=True):
         super().__init__()
         self.scale = dim ** -0.5
         self.eps = eps
         self.g = nn.Parameter(torch.ones(dim))
-        self.scale_shift_process = nn.Linear(dim, dim * 2)
+        self.scale_shift_process = nn.Linear(dim, dim * 2, bias=bias)
 
     def forward(self, x, norm_scale_shift_inp):
         norm = torch.norm(x, dim=-1, keepdim=True) * self.scale
